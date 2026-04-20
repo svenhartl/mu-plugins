5d7f58c8711825202559fcf0bd9bc5bdaf146955
Sven Hartl <sven.hartl5@gmail.com>
2026-04-20 14:52:51 +0200

    posttypes

---
+++ post-types.php
@@ -0,0 +1,51 @@
+<?php
+ 
+function university_post_types() {
+
+  //event post type
+
+  register_post_type('event', array(
+    'supports' => array(
+      'excerpt','title','editor'
+    ),
+    'rewrite' => array(
+      'slug' => 'events'
+    ),
+    'has_archive' => true,
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => 'Add New Event',
+      'edit_item' => 'Edit Event',
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar'
+  ));
+
+  //program post type
+
+  register_post_type('program', array(
+    'supports' => array(
+      'title','editor'
+    ),
+    'rewrite' => array(
+      'slug' => 'programs'
+    ),
+    'has_archive' => true,
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));
+
+}
+ 
+add_action('init', 'university_post_types');
